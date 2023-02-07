5a9116a707b5a0dd2400a7d6178b4c9bf1795d83
leucas <sodapanda20@gmail.com>
2023-02-08 03:22:32 +0800

    [add]

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,76 @@
 <script>
 	import { onMount } from 'svelte';
-	import { Button } from '@svelteuidev/core';
-	import { of, map } from 'rxjs';
+	import { Switch, Notification } from '@svelteuidev/core';
 
-	let msg = '';
+	let ip = '0.0.0.0';
+	let isProxy = false;
+
 	onMount(() => {
 		console.log('mount');
+		getMyIp();
+		checkProxyStatus();
 	});
 
-	function handleClick() {
-		of('1', '2', '3')
-			.pipe(map((text) => `${msg} ${text}`))
-			.subscribe((v) => {
-				msg = v;
+	async function handleSwitch(event) {
+		let newState = event.target.checked;
+		console.log(newState);
+		let cmd = '';
+		if (newState) {
+			cmd = `ip rule delete from ${ip} to 0/0 table qishui`;
+		} else {
+			cmd = `ip rule add from ${ip} lookup qishui`;
+		}
+		console.log(cmd);
+
+		await updateProxyStatus(cmd);
+
+		checkProxyStatus();
+	}
+
+	async function getMyIp() {
+		try {
+			let rsp = await fetch('http://192.168.7.1:8080/myip');
+			let rspJson = await rsp.json();
+			ip = rspJson.ip;
+		} catch (error) {
+			alert('error');
+		}
+	}
+
+	async function checkProxyStatus() {
+		try {
+			let rsp = await fetch('http://192.168.7.1:8080/command', {
+				method: 'POST',
+				body: 'ip rule'
 			});
+			let data = await rsp.json();
+			const outputStr = String(data.output);
+			console.log(outputStr);
+			const myIPStr = String(data.ip);
+
+			isProxy = outputStr.includes(myIPStr) ? false : true;
+		} catch (error) {
+			alert('error');
+		}
+	}
+
+	async function updateProxyStatus(commandStr) {
+		let rsp = await fetch('http://192.168.7.1:8080/command', {
+			method: 'POST',
+			body: commandStr
+		});
+
+		let data = await rsp.json();
+		console.log(data);
 	}
 </script>
 
-<Button on:click={handleClick}>Settings</Button>
-{msg}
+<div class="bg-slate-100 flex flex-col items-center justify-center min-h-screen w-screen">
+	<div class="text-stone-900 font-black text-xl">
+		{ip}
+	</div>
+	<Switch bind:checked={isProxy} on:change={handleSwitch} class="mt-6" size="xl" />
+	<Notification title="当前状态" class="mt-6" withCloseButton={false}>
+		{isProxy ? '代理' : '直连'}
+	</Notification>
+</div>
